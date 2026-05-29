8b00873ebc774516c7bc593e9b9eb27d64ea1c44
Marguel741 <marguel741@gmail.com>
2026-05-29 18:34:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const buildDate = new Date().toISOString().slice(0, 10).replace(/-/g, '.');
     return {
+      define: {
+        __APP_VERSION__: JSON.stringify(`v${buildDate}`),
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
