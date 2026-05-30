7b71bfebb9714293362b5bfafd81280be036eae4
Marguel741 <marguel741@gmail.com>
2026-05-31 00:24:27 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as Sentry from '@sentry/react';
 import App from './App';
-
-Sentry.init({
-  dsn: "https://69c998a897eb9eb49d2d1e1a5b103192@o4511280052305920.ingest.us.sentry.io/4511280060170240",
-  environment: "production",
-  tracesSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [Sentry.replayIntegration({
-    maskAllInputs: true,
-    blockAllMedia: false,
-  })],
-  beforeSend(event) {
-    const mode = localStorage.getItem('mg_diagnostic_mode');
-    if (mode === 'NEVER') return null;
-    return event;
-  },
-});
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element to mount to");
