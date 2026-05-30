6007c7b099c5d4a4f2d36b7063ca6e4450392178
Marguel741 <marguel741@gmail.com>
2026-05-30 23:25:52 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const prodConfig = {
   apiKey: "AIzaSyAgjxTNrME13qW_BsieJ6nziY9_2yDsxPU",
@@ -23,3 +25,4 @@ const isProd = import.meta.env.VITE_ENV === 'production';
 const app = initializeApp(isProd ? prodConfig : testConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
